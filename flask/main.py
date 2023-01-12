faa4a7dc73aeef99ea7bd73b604cfdfead533ab4
pelegov <nave.peleg@gmail.com>
2023-01-12 16:18:52 +0200

    flask

---
+++ flask/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+import os
+import signal
+import docker
+
+# client = docker.from_env()
+# containers = client.containers.list(all=True)
+
+# for x in containers:
+#     print(x)
+    
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+@app.route('/list')
+def list():
+    list = []
+    client = docker.from_env()
+    containers = client.containers.list(all=True)
+    return jsonify([d.name for d in containers])
+            
+
+@app.route('/stop_server')
+def stop_server():
+    os.kill(os.getpid(), signal.SIGKILL)
+    return 'Server Stopped'
+
+app.run(host='0.0.0.0', port=5000)
