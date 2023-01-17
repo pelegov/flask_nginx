b6470e640ddff0292e297cdf6ecdf9612d3e854a
pelegov <nave.peleg@gmail.com>
2023-01-17 23:30:23 +0200

    last step

--- run-app.sh
+++ run-app.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-docker-compose --no-ansi up -d
+docker-compose up -d
 
+###check status
+status=$(curl -I http://localhost:80 2> /dev/null | head -n 1 | cut -d$' ' -f2)
+if [ $status -eq 200]; 
+then
+    echo "success"
+else
+    echo "fail"
+fi
+
+docker-compose down --rmi all
